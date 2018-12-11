ded48a753ba377854725c65cba1190a8b77e918a
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-11 18:39:33 +0100

    Update ejercicio2.java

--- ejercicio2.java
+++ ejercicio2.java
@@ -5,25 +5,27 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class ejercicio2 {
+public class exercicio2 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int i;
 
 		FileInputStream filein= new FileInputStream ("/home/cf17eric.visier/Escriptori/FicheroDatos.dat");
 		DataInputStream dataIS = new DataInputStream (filein);
 
 
-			try {
-				while (true) {
-					System.out.println("Nombre: " + dataIS.readUTF() + " Edat: " + dataIS.readInt());
-				}
-			} catch(EOFException e) {
-				System.out.println("END OF FILE");
-
+		try {
+			while (true) {
+				System.out.println("Nombre: " + dataIS.readUTF() + " Edat: " + dataIS.readInt());
 			}
+		} catch(EOFException e) {
+			System.out.println("END OF FILE");
 
-
+		} finally {
+			filein.close();
 		}
+
+
 	}
+}
+
