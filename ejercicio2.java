a53bd436ceedc910f3451f7730d45e278a70f5d4
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-15 18:33:20 +0100

    Update ejercicio2.java

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,3 +1,29 @@
+/*
+ * exercicio2.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * Ahora escribe un programa que permita visualizar los datos grabados anteriormente en el fichero FicheroDatos.dat. Se deben obtener en el mismo orden en el que se escribieron, es decir, primero obtenemos el nombre y luego la edad
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package FicherosBinarios;
 
 import java.io.DataInputStream;
